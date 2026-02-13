9788e3f7e2e1e4da789d81a0d9aa5e58a8001579
sujeetrana123-byte <sujeetranassm123@gmail.com>
2026-02-13 10:33:24 +0530

    Add program to check for leap year

---
+++ Program to check if a number is leap year or not.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int year;
+    printf("Enter Year: ");
+    scanf("%d",&year);
+    if(year%4==0){
+        if(year%100!=0){
+            printf("The year is a leap year.");
+        } else if(year%400==0){
+            printf("The year is a leap year");
+
+        }
+        }else{
+            printf("The year is not a leap year.");
+    }
+
+
+}
